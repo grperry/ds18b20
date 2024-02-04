c1be43f2fe6ace066ea72f12aee2f9e7af7e96a0
grperry <92124381+grperry@users.noreply.github.com>
2024-02-04 19:28:39 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ BaudRate.BaudRate115200
 )
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
-    control.waitMicros(500)
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    control.waitMicros(480)
     serial.writeLine("" + (pins.pulseIn(DigitalPin.P0, PulseValue.Low)))
 })
